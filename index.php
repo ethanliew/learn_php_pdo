cfcd0ff05f9f50a72e7179da4b080abec190b417
Ethan Liew <liewyeeling@gmail.com>
2014-01-16 16:51:23 +0800

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Ethan Liew
+ * Project  :
+ * Date     : 1/16/14
+ * Time     : 4:50 PM
+ * Version  : 
+ * Desc     :
+ *
+ *
+ */
+//https://github.com/ethanliew/learn_php_pdo
